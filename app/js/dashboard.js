4192794f4a6dce706dffa46b8cc1263415995463
Laurens De Cock <laurens.decock@student.odisee.be>
2016-09-11 21:40:25 +0200

    Update api result format and show data in view in ugly fashion

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1,51 +1,51 @@
-(function() {
-  // API endpoint
-  // http://localhost:1234/api/briefings
+(function () {
+    // API endpoint
+    // http://localhost:1234/api/briefings
 
-  var runningAjaxCalls = [];
+    var runningAjaxCalls = [];
 
-  new Vue({
-    el: '#briefingApp',
-    data: {
-      counter_media_ids: 0,
-      errors: [],
-      newBriefing: '',
-      briefings: [],
-      briefingIds: []
-    },
-    created: function() { 
-      refreshView(this);
-    },
-    methods: {
-      addBriefing: function () {
-        var text = this.newBriefing.trim();
-        if (text) {
-          this.briefings.push({ text: text });
-          this.newBriefing = '';
+    new Vue({
+        el: '#briefingApp',
+        data: {
+            counter_media_ids: 0,
+            errors: [],
+            newBriefing: '',
+            briefings: [],
+            //  briefingIds: []
+        },
+        created: function () {
+            refreshView(this);
+        },
+        methods: {
+            addBriefing: function () {
+                var text = this.newBriefing.trim();
+                if (text) {
+                    this.briefings.push({ text: text });
+                    this.newBriefing = '';
+                }
+            },
+            removeBriefing: function (index) {
+                this.briefings.splice(index, 1);
+            },
+            addInput: function (divName) {
+                var newdiv = document.createElement('div');
+                newdiv.innerHTML = "<li>Entry " + (this.counter_media_ids + 1) + " <br><input type='text' name='myInputs[]'></li>";
+                document.getElementById(divName).appendChild(newdiv);
+            }
         }
-      },
-      removeBriefing: function (index) {
-        this.briefings.splice(index, 1);
-      },
-      addInput: function(divName) {
-          var newdiv = document.createElement('div');
-          newdiv.innerHTML = "<li>Entry " + (this.counter_media_ids + 1) + " <br><input type='text' name='myInputs[]'></li>";
-          document.getElementById(divName).appendChild(newdiv);
-      }
-    }
-  });
+    });
 
     // Refresh the whole view
-    function refreshView(vueinstance){
-      // Read briefings
-      runningAjaxCalls.push(ajaxcall("/api/briefings", function(err, result) {
-        if(err) { vueinstance.errors.push(err); }
-        else {     
-          errors = [];
-          vueinstance.briefings = result; // For loader (?)
-          vueinstance.briefingIds = Object.keys(result);
-        }
-      }));
+    function refreshView(vueinstance) {
+        // Read briefings
+        runningAjaxCalls.push(ajaxcall("/api/briefings", function (err, result) {
+            if (err) { vueinstance.errors.push(err); }
+            else {
+                errors = [];
+                vueinstance.briefings = result.briefings;
+
+            }
+        }));
     }
 
 })();
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,77 +3,117 @@ var moment = require('moment');
 var jsend = require('../util/jsend');
 
 module.exports = function (router, config, request) {
-  router.get('/api/briefings/:id?', briefings);
+    router.get('/api/briefings/:id?', briefings);
 
-  function briefings (req, res, next) {
-    if (!req.params.id) req.params.id = '';
-    var url = config.muleHost + '/briefings/' + req.params.id;
+    function briefings(req, res, next) {
+        if (!req.params.id) req.params.id = '';
+        var url = config.muleHost + '/briefings/' + req.params.id;
 
-    debugger;
-    //forwardRequestCall(url, res, next);
+        debugger;
+        //forwardRequestCall(url, res, next);
 
-    var data = [{
-            "briefing123": [
-              {
-                "briefing_title": "Beeldmateriaal test",
-                "media_type": "video",
-                "retry_count": null,
-                "media_id": "AIM10022093",
-                "briefing_id": "briefing123",
-                "email": "brecht.vandevyvere@viaa.be",
-                "status": "QUEUED",
-                "reception_date": "2016-08-25T10:52:37",
-                "id": 2
-              },
-              {
-                "briefing_title": "Beeldmateriaal test",
-                "media_type": "audio",
-                "retry_count": null,
-                "media_id": "AIM10022092",
-                "briefing_id": "briefing123",
-                "email": "brecht.vandevyvere@viaa.be",
-                "status": "REQUESTED",
-                "reception_date": "2016-08-25T10:52:37",
-                "id": 3
-              }
-            ]
-          },
-          {
-            "briefing456": [
-              {
-                "briefing_title": "joo",
-                "media_type": "video",
-                "retry_count": null,
-                "media_id": "AIM1002",
-                "briefing_id": "briefing456",
-                "email": "brecht.vandevyvere@viaa.be",
-                "status": "RECONCILED",
-                "reception_date": "2016-08-25T10:52:37",
-                "id": 4
-              }
-            ]
-          }];
+        var data = {
+            "briefings": [
+                {
+                    "briefing_id": "briefing 1",
+                    "briefing_title": "Beeldmateriaal test",
+                    "reception_date": "2016-08-25T10:52:37",
+                    "media_ids": [
+                        {
+                            "media_id": "AIM10022093",
+                            "media_type": "video",
+                            "status": "QUEUED",
+                            "email": "brecht.vandevyvere@viaa.be",
+                            "retry_count": null,
+                        },
+                        {
+                            "media_id": "AIM10022093",
+                            "media_type": "video",
+                            "status": "REQUESTED",
+                            "email": "hendrik@viaa.be",
+                            "retry_count": null,
+                        }]
+                },
+                {
+                    "briefing_id": "briefing 2",
+                    "briefing_title": "Beeldmateriaal test",
+                    "reception_date": "2016-08-25T10:52:37",
+                    "media_ids": [
+                        {
+                            "media_id": "AIM10022093",
+                            "media_type": "video",
+                            "status": "QUEUED",
+                            "email": "brecht.vandevyvere@viaa.be",
+                            "retry_count": null,
+                        }]
+                }]
+        };
 
-    res
-        .append('Content-Type', 'application/json')
-        .send(jsend.success(data));
-  }
 
-  function forwardRequestCall (url, res, next, parse) {
-    console.log('executing forwardRequestCall(' + url + ')');
-    request(url, function (error, response, body) {
-      if (error) return next(error);
-      if (response.statusCode != 200) return next(jsend.error(response.statusCode, error));
 
-      if (typeof body === 'string') {
-        body = JSON.parse(body);
-      }
 
-      data = parse ? parse(body) : body;
+        // {
+        //     "briefing123": [
+        //       {
+        //         "briefing_title": "Beeldmateriaal test",
+        //         "media_type": "video",
+        //         "retry_count": null,
+        //         "media_id": "AIM10022093",
+        //         "briefing_id": "briefing123",
+        //         "email": "brecht.vandevyvere@viaa.be",
+        //         "status": "QUEUED",
+        //         "reception_date": "2016-08-25T10:52:37",
+        //         "id": 2
+        //       },
+        //       {
+        //         "briefing_title": "Beeldmateriaal test",
+        //         "media_type": "audio",
+        //         "retry_count": null,
+        //         "media_id": "AIM10022092",
+        //         "briefing_id": "briefing123",
+        //         "email": "brecht.vandevyvere@viaa.be",
+        //         "status": "REQUESTED",
+        //         "reception_date": "2016-08-25T10:52:37",
+        //         "id": 3
+        //       }
+        //     ]
+        //   },
+        //   {
+        //     "briefing456": [
+        //       {
+        //         "briefing_title": "joo",
+        //         "media_type": "video",
+        //         "retry_count": null,
+        //         "media_id": "AIM1002",
+        //         "briefing_id": "briefing456",
+        //         "email": "brecht.vandevyvere@viaa.be",
+        //         "status": "RECONCILED",
+        //         "reception_date": "2016-08-25T10:52:37",
+        //         "id": 4
+        //       }
+        //     ]
+        //   }];
 
-      res
-        .append('Content-Type', 'application/json')
-        .send(jsend.success(data));
-    });
-  }
+        res
+            .append('Content-Type', 'application/json')
+            .send(jsend.success(data));
+    }
+
+    function forwardRequestCall(url, res, next, parse) {
+        console.log('executing forwardRequestCall(' + url + ')');
+        request(url, function (error, response, body) {
+            if (error) return next(error);
+            if (response.statusCode != 200) return next(jsend.error(response.statusCode, error));
+
+            if (typeof body === 'string') {
+                body = JSON.parse(body);
+            }
+
+            data = parse ? parse(body) : body;
+
+            res
+                .append('Content-Type', 'application/json')
+                .send(jsend.success(data));
+        });
+    }
 };
