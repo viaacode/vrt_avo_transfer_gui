ccb418ae57548ba34201b5ae86da1485a741ec03
Laurens De Cock <laurens.decock@student.odisee.be>
2016-09-13 15:29:59 +0200

    Add tag style media id input field, post email, briefing_id, and media ids to post request

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     // http://localhost:1234/api/briefings
 
     var runningAjaxCalls = [];
+    var mediaIdsDelimiter = ',';
 
     new Vue({
         el: '#briefingApp',
@@ -18,21 +19,6 @@ $(document).ready(function () {
             
         },
         methods: {
-            addBriefing: function () {
-                var text = this.newBriefing.trim();
-                if (text) {
-                    this.briefings.push({ text: text });
-                    this.newBriefing = '';
-                }
-            },
-            removeBriefing: function (index) {
-                this.briefings.splice(index, 1);
-            },
-            addInput: function (divName) {
-                var newdiv = document.createElement('div');
-                newdiv.innerHTML = "<li>Entry " + (this.counter_media_ids + 1) + " <br><input type='text' name='myInputs[]'></li>";
-                document.getElementById(divName).appendChild(newdiv);
-            },
             toggleContent: function(e) {
                 $(e.target).closest('.briefingItem').find('.briefing_content').toggle(150);
             },
@@ -40,12 +26,21 @@ $(document).ready(function () {
                 $('.addItem-content').show(300);
                 $('.addItem-icon').hide(300);
             },
+            // Toast for if we would not redirect to other page
             submitAddItem: function() {
                 $('.addItem-content').hide(300);
                 $('.addItem-icon').show(300);
 
                 $('.toast').text('Item toegevoegd');
                 $('.toast').slideDown(300).delay(3000).slideUp(300);
+            },
+            addMediaId: function() {
+                var val = $('.mediaIdValue').val();
+                $('.media-ids-added').append('<span>' + val + '</span>');
+
+                // Add to hidden field for POST request
+                if($('#mediaIds').val() === '') $('#mediaIds').val(val);    // If first item, don't add delimiter in front of it
+                else $('#mediaIds').val($('#mediaIds').val() + mediaIdsDelimiter + val);
             }
         }
     });
